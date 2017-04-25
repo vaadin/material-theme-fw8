cd905cec5677a4bc75423c0dd0ada7c1dcec4584
Martin Vysny <mavi@vaadin.com>
2017-04-25 11:53:37 +0300

    NavigationItem refactoring to get rid of NPEs

--- src/main/java/org/test/MyUI.java
+++ src/main/java/org/test/MyUI.java
@@ -2,10 +2,13 @@ package org.test;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.Resource;
 import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import org.test.components.NavigationDrawer;
 import org.test.components.NavigationItem;
@@ -19,24 +22,10 @@ import javax.servlet.annotation.WebServlet;
 @Theme("material-design")
 public class MyUI extends UI {
 
-    public static final String BUTTONS = "Buttons";
-    public static final String TEXTFIELDS = "Text fields";
-    public static final String DATA_TABLES = "Data tables";
-    public static final String DIALOGS = "Dialogs";
-    public static final String SNACKBARS_TOASTS = "Snackbars & toasts";
-    public static final String TABS = "Tabs";
-    public static final String SELECTION_CONTROLS = "Selection controls";
-    public static final String PICKERS = "Pickers";
-    public static final String MENUS = "Menus";
-    public static final String TOOLTIPS = "Tooltips";
-    public static final String TYPOGRAPHY = "Typography";
-    public static final String COLORS = "Colors";
-
     private CssLayout root;
     private AppBar appBar = new AppBar();
     private CssLayout content = new CssLayout();
     private NavigationDrawer navigationDrawer = new NavigationDrawer();
-    private String LISTS;
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
@@ -64,57 +53,45 @@ public class MyUI extends UI {
         content.addComponent(card);
     }
 
+    private void addNavItem(Resource icon, String caption, Component view) {
+        final NavigationItem item = new NavigationItem(icon, caption, view);
+        item.addClickListener(event -> {
+            navigationDrawer.selectNavigationItem(item);
+            content.removeAllComponents();
+            content.addComponent(item.getView());
+        });
+        navigationDrawer.addNavigationItem(item);
+    }
+
     private void initNavigationItems() {
-        LISTS = "Lists";
-        for (String s : new String[]{"Bottom navigation", "Bottom sheets", BUTTONS, "Buttons: Floating Action Button", "Cards", "Chips", COLORS, DATA_TABLES, DIALOGS, "Dividers", "Expansion panels", "Grid lists", LISTS, "Lists: Controls", MENUS, PICKERS, "Progress & activity", SELECTION_CONTROLS, "Sliders", SNACKBARS_TOASTS, "Steppers", "Subheaders", TABS, TEXTFIELDS, "Toolbars", TOOLTIPS, TYPOGRAPHY, "Widgets"}) {
-            NavigationItem item = new NavigationItem(s);
-            navigationDrawer.addNavigationItem(item);
-            item.addClickListener(event -> {
-                navigationDrawer.selectNavigationItem(item);
-                content.removeAllComponents();
-                content.addComponent(item.getView());
-            });
-            if (s.equals(BUTTONS)) {
-                item.setIcon(MaterialIcons.FILE_DOWNLOAD);
-                item.setView(new ButtonsView());
-            } else if (s.equals(TEXTFIELDS)) {
-                item.setIcon(MaterialIcons.TEXT_FIELDS);
-                item.setView(new TextFieldsView());
-            } else if (s.equals(DATA_TABLES)) {
-                item.setIcon(MaterialIcons.GRID_ON);
-                item.setView(new DataTablesView());
-            } else if (s.equals(SELECTION_CONTROLS)) {
-                item.setIcon(MaterialIcons.CHECK_BOX);
-                item.setView(new SelectionControlsView());
-            } else if (s.equals(PICKERS)) {
-                item.setIcon(MaterialIcons.EVENT);
-                item.setView(new PickersView());
-            } else if (s.equals(MENUS)) {
-                item.setIcon(MaterialIcons.MENU);
-                item.setView(new MenusView());
-            } else if (s.equals(TOOLTIPS)) {
-                item.setIcon(MaterialIcons.HELP);
-                item.setView(new TooltipsView());
-            } else if (s.equals(TABS)) {
-                item.setIcon(MaterialIcons.TAB);
-                item.setView(new TabsView());
-            } else if (s.equals(DIALOGS)) {
-                item.setIcon(MaterialIcons.DESKTOP_WINDOWS);
-                item.setView(new DialogsView());
-            } else if (s.equals(SNACKBARS_TOASTS)) {
-                item.setIcon(MaterialIcons.DESKTOP_WINDOWS);
-                item.setView(new SnackbarsToastsView());
-            } else if (s.equals(TYPOGRAPHY)) {
-                item.setIcon(MaterialIcons.TEXT_FORMAT);
-                item.setView(new TypographyView());
-            } else if (s.equals(COLORS)) {
-                item.setIcon(MaterialIcons.PALETTE);
-                item.setView(new ColorsView());
-            } else if (s.equals(LISTS)) {
-                item.setIcon(MaterialIcons.LIST);
-                item.setView(new ListsView());
-            }
-        }
+        addNavItem(null, "Bottom navigation", new Label("Todo implement"));
+        addNavItem(null, "Bottom sheets", new Label("Todo implement"));
+        addNavItem(MaterialIcons.FILE_DOWNLOAD, "Buttons", new ButtonsView());
+        addNavItem(null, "Buttons: Floating Action Button", new Label("Todo implement"));
+        addNavItem(null, "Cards", new Label("Todo implement"));
+        addNavItem(null, "Chips", new Label("Todo implement"));
+        addNavItem(MaterialIcons.PALETTE, "Colors", new ColorsView());
+        addNavItem(MaterialIcons.GRID_ON, "Data tables", new DataTablesView());
+        addNavItem(MaterialIcons.DESKTOP_WINDOWS, "Dialogs", new DialogsView());
+        addNavItem(null, "Dividers", new Label("Todo implement"));
+        addNavItem(null, "Expansion panels", new Label("Todo implement"));
+        addNavItem(null, "Grid lists", new Label("Todo implement"));
+        addNavItem(MaterialIcons.LIST, "Lists", new ListsView());
+        addNavItem(null, "Lists: Controls", new Label("Todo implement"));
+        addNavItem(MaterialIcons.MENU, "Menus", new MenusView());
+        addNavItem(MaterialIcons.EVENT, "Pickers", new PickersView());
+        addNavItem(null, "Progress & activity", new Label("Todo implement"));
+        addNavItem(MaterialIcons.CHECK_BOX, "Selection controls", new SelectionControlsView());
+        addNavItem(null, "Sliders", new Label("Todo implement"));
+        addNavItem(MaterialIcons.DESKTOP_WINDOWS, "Snackbars & toasts", new SnackbarsToastsView());
+        addNavItem(null, "Steppers", new Label("Todo implement"));
+        addNavItem(null, "Subheaders", new Label("Todo implement"));
+        addNavItem(MaterialIcons.TAB, "Tabs", new TabsView());
+        addNavItem(MaterialIcons.TEXT_FIELDS, "Text fields", new TextFieldsView());
+        addNavItem(null, "Toolbars", new Label("Todo implement"));
+        addNavItem(MaterialIcons.HELP, "Tooltips", new TooltipsView());
+        addNavItem(MaterialIcons.TEXT_FORMAT, "Typography", new TypographyView());
+        addNavItem(null, "Widgets", new Label("Todo implement"));
     }
 
 
