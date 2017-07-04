fd8ace731daa7e8934d6f70e82b77527f3bad6ab
anezthes <joacim@vaadin.com>
2017-07-04 16:24:41 +0300

    More minimums and maximums. :)

--- src/main/java/org/vaadin/layout/MaxWidths.java
+++ src/main/java/org/vaadin/layout/MaxWidths.java
@@ -4,6 +4,7 @@ package org.vaadin.layout;
  * Created by jonte on 03/04/2017.
  */
 public class MaxWidths {
+    public static final String MW_400 = new String("max-width-400");
     public static final String MW_720 = new String("max-width-720");
     public static final String MW_1024 = new String("max-width-1024");
     public static final String MW_1104 = new String("max-width-1104");
---
+++ src/main/java/org/vaadin/layout/MinHeights.java
@@ -0,0 +1,8 @@
+package org.vaadin.layout;
+
+/**
+ * Created by jonte on 30/06/2017.
+ */
+public class MinHeights {
+    public static final String MH_FULL = new String("min-height-full");
+}
