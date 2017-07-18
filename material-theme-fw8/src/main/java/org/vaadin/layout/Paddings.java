218968cbb99aef0770c4b5a6d930fb63f4851193
anezthes <joacim@vaadin.com>
2017-07-18 16:14:24 +0300

    Moar paddings.

--- material-theme-fw8/src/main/java/org/vaadin/layout/Paddings.java
+++ material-theme-fw8/src/main/java/org/vaadin/layout/Paddings.java
@@ -5,20 +5,20 @@ package org.vaadin.layout;
  */
 public class Paddings {
     public static class All {
+        public static final String AUTO = new String("pad-a-a");
         public static final String SMALL = new String("pad-a-s");
         public static final String MEDIUM = new String("pad-a-m");
         public static final String LARGE = new String("pad-a-l");
-
         public static final String X_LARGE = new String("pad-a-xl");
         public static final String XX_LARGE = new String("pad-a-xxl");
         public static final String XXX_LARGE = new String("pad-a-xxxl");
-
         public static final String XXXX_LARGE = new String("pad-a-xxxxl");
         public static final String XXXXX_LARGE = new String("pad-a-xxxxxl");
         public static final String XXXXXX_LARGE = new String("pad-a-xxxxxxl");
     }
 
     public static class Horizontal {
+        public static final String AUTO = new String("pad-h-a");
         public static final String SMALL = new String("pad-h-s");
         public static final String MEDIUM = new String("pad-h-m");
         public static final String LARGE = new String("pad-h-l");
@@ -31,6 +31,7 @@ public class Paddings {
     }
 
     public static class Vertical {
+        public static final String AUTO = new String("pad-v-a");
         public static final String SMALL = new String("pad-v-s");
         public static final String MEDIUM = new String("pad-v-m");
         public static final String LARGE = new String("pad-v-l");
@@ -44,42 +45,54 @@ public class Paddings {
     }
 
     public static class Top {
+        public static final String AUTO = new String("pad-t-a");
         public static final String SMALL = new String("pad-t-s");
         public static final String MEDIUM = new String("pad-t-m");
         public static final String LARGE = new String("pad-t-l");
-
         public static final String X_LARGE = new String("pad-t-xl");
         public static final String XX_LARGE = new String("pad-t-xxl");
         public static final String XXX_LARGE = new String("pad-t-xxxl");
-
         public static final String XXXX_LARGE = new String("pad-t-xxxxl");
         public static final String XXXXX_LARGE = new String("pad-t-xxxxxl");
         public static final String XXXXXX_LARGE = new String("pad-t-xxxxxxl");
     }
 
     public static class Right {
+        public static final String AUTO = new String("pad-r-a");
         public static final String SMALL = new String("pad-r-s");
         public static final String MEDIUM = new String("pad-r-m");
         public static final String LARGE = new String("pad-r-l");
+        public static final String X_LARGE = new String("pad-r-xl");
+        public static final String XX_LARGE = new String("pad-r-xxl");
+        public static final String XXX_LARGE = new String("pad-r-xxxl");
+        public static final String XXXX_LARGE = new String("pad-r-xxxxl");
+        public static final String XXXXX_LARGE = new String("pad-r-xxxxxl");
+        public static final String XXXXXX_LARGE = new String("pad-r-xxxxxxl");
     }
 
     public static class Bottom {
+        public static final String AUTO = new String("pad-b-a");
         public static final String SMALL = new String("pad-b-s");
         public static final String MEDIUM = new String("pad-b-m");
         public static final String LARGE = new String("pad-b-l");
-
         public static final String X_LARGE = new String("pad-b-xl");
         public static final String XX_LARGE = new String("pad-b-xxl");
         public static final String XXX_LARGE = new String("pad-b-xxxl");
-
         public static final String XXXX_LARGE = new String("pad-b-xxxxl");
         public static final String XXXXX_LARGE = new String("pad-b-xxxxxl");
         public static final String XXXXXX_LARGE = new String("pad-b-xxxxxxl");
     }
 
     public static class Left {
+        public static final String AUTO = new String("pad-l-a");
         public static final String SMALL = new String("pad-l-s");
         public static final String MEDIUM = new String("pad-l-m");
         public static final String LARGE = new String("pad-l-l");
+        public static final String X_LARGE = new String("pad-l-xl");
+        public static final String XX_LARGE = new String("pad-l-xxl");
+        public static final String XXX_LARGE = new String("pad-l-xxxl");
+        public static final String XXXX_LARGE = new String("pad-l-xxxxl");
+        public static final String XXXXX_LARGE = new String("pad-l-xxxxxl");
+        public static final String XXXXXX_LARGE = new String("pad-l-xxxxxxl");
     }
 }
