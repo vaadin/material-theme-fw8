bd7dcb3ab0a9dba5c5c12ae0097c9589e73efec2
Enver Haase <enver@vaadin.com>
2017-08-11 12:47:25 +0200

    * improve API somewhat, but break BTCS project.

--- material-theme-fw8/src/main/java/org/vaadin/components/DataTableHeader.java
+++ material-theme-fw8/src/main/java/org/vaadin/components/DataTableHeader.java
@@ -13,6 +13,10 @@ import org.vaadin.style.MaterialColor;
 import org.vaadin.style.MaterialIcons;
 import org.vaadin.style.Typography;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by jonte on 17/03/2017.
  */
@@ -23,6 +27,33 @@ public class DataTableHeader extends FlexLayout {
     private final Button filter;
     private final Button more;
     private final Button delete;
+
+    public enum HeaderButton {FILTER, MORE, DELETE};
+
+    private List<HeaderButton> enabledButtons;
+
+    public void enableButtons(HeaderButton... enabledButtons){
+        this.enabledButtons = Arrays.asList(enabledButtons);
+
+        filter.setEnabled(false);
+        more.setEnabled(false);
+        delete.setEnabled(false);
+        for (HeaderButton bs : this.enabledButtons){
+            switch (bs){
+                case MORE:
+                    more.setEnabled(true);
+                    break;
+                case DELETE:
+                    delete.setEnabled(true);
+                    break;
+                case FILTER:
+                    filter.setEnabled(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
     public DataTableHeader(String title) {
         setAlignItems(FlexLayout.AlignItems.CENTER);
@@ -46,21 +77,33 @@ public class DataTableHeader extends FlexLayout {
         addComponents(this.titleLabel, filter, delete, more);
     }
 
-    public Button getFilterButton(){
-        return filter;
-    }
-
-    public Button getMoreButton(){
-        return more;
-    }
-
-    public Button getDeleteButton(){
-        return delete;
-    }
+//    public Button getFilterButton(){
+//        return filter;
+//    }
+//
+//    public Button getMoreButton(){
+//        return more;
+//    }
+//
+//    public Button getDeleteButton(){
+//        return delete;
+//    }
 
     public void setTitle(String title){
         this.title = title;
         this.titleLabel.setValue(title);
+    }
+
+    public void addFilterButtonStyleName(String styleName){
+        this.filter.addStyleName(styleName);
+    }
+
+    public void removeFilterButtonStyleName(String styleName){
+        this.filter.removeStyleName(styleName);
+    }
+
+    public void addFilterButtonClickListener(Button.ClickListener listener){
+        this.filter.addClickListener(listener);
     }
 
     public void setGrid(Grid grid) {
@@ -71,16 +114,28 @@ public class DataTableHeader extends FlexLayout {
                 titleLabel.removeStyleName(Typography.Dark.Table.Title.PRIMARY);
                 titleLabel.addStyleName(Typography.Dark.Subheader.PRIMARY + " " + MaterialColor.BLUE_500.getFontColorStyle());
 
-                filter.setVisible(false);
-                delete.setVisible(true);
+                if (enabledButtons.contains(HeaderButton.FILTER)) {
+                    filter.setVisible(false);
+                }
+                if (enabledButtons.contains(HeaderButton.DELETE)){
+                    delete.setVisible(true);
+                }
+
                 addStyleName(MaterialColor.BLUE_50.getBackgroundColorStyle());
             } else {
                 titleLabel.setValue(title);
                 titleLabel.addStyleName(Typography.Dark.Table.Title.PRIMARY);
                 titleLabel.removeStyleName(Typography.Dark.Subheader.PRIMARY + " " + MaterialColor.BLUE_500.getFontColorStyle());
 
-                filter.setVisible(true);
-                delete.setVisible(false);
+
+                if (enabledButtons.contains(HeaderButton.FILTER)) {
+                    filter.setVisible(true);
+                }
+
+                if (enabledButtons.contains(HeaderButton.DELETE)) {
+                    delete.setVisible(false);
+                }
+
                 removeStyleName(MaterialColor.BLUE_50.getBackgroundColorStyle());
             }
         });
