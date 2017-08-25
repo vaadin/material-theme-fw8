d0ad15ed729df8719f8c36edd459098a23913ef0
anezthes <joacim@vaadin.com>
2017-08-25 11:39:08 +0300

    I need more spacing!

--- material-theme-fw8/src/main/java/org/vaadin/layout/Spacings.java
+++ material-theme-fw8/src/main/java/org/vaadin/layout/Spacings.java
@@ -8,41 +8,62 @@ public class Spacings {
         public static final String SMALL = new String("spacing-a-s");
         public static final String MEDIUM = new String("spacing-a-m");
         public static final String LARGE = new String("spacing-a-l");
+        public static final String X_LARGE = new String("spacing-a-xl");
+        public static final String XX_LARGE = new String("spacing-a-xxl");
+        public static final String XXX_LARGE = new String("spacing-a-xxxl");
     }
 
     public static class Horizontal {
         public static final String SMALL = new String("spacing-h-s");
         public static final String MEDIUM = new String("spacing-h-m");
         public static final String LARGE = new String("spacing-h-l");
+        public static final String X_LARGE = new String("spacing-h-xl");
+        public static final String XX_LARGE = new String("spacing-h-xxl");
+        public static final String XXX_LARGE = new String("spacing-h-xxxl");
     }
 
     public static class Vertical {
         public static final String SMALL = new String("spacing-v-s");
         public static final String MEDIUM = new String("spacing-v-m");
         public static final String LARGE = new String("spacing-v-l");
+        public static final String X_LARGE = new String("spacing-v-xl");
+        public static final String XX_LARGE = new String("spacing-v-xxl");
+        public static final String XXX_LARGE = new String("spacing-v-xxxl");
     }
 
     public static class Top {
         public static final String SMALL = new String("spacing-t-s");
         public static final String MEDIUM = new String("spacing-t-m");
         public static final String LARGE = new String("spacing-t-l");
+        public static final String X_LARGE = new String("spacing-t-xl");
+        public static final String XX_LARGE = new String("spacing-t-xxl");
+        public static final String XXX_LARGE = new String("spacing-t-xxxl");
     }
 
     public static class Right {
         public static final String SMALL = new String("spacing-r-s");
         public static final String MEDIUM = new String("spacing-r-m");
         public static final String LARGE = new String("spacing-r-l");
+        public static final String X_LARGE = new String("spacing-r-xl");
+        public static final String XX_LARGE = new String("spacing-r-xxl");
+        public static final String XXX_LARGE = new String("spacing-r-xxxl");
     }
 
     public static class Bottom {
         public static final String SMALL = new String("spacing-b-s");
         public static final String MEDIUM = new String("spacing-b-m");
         public static final String LARGE = new String("spacing-b-l");
+        public static final String X_LARGE = new String("spacing-b-xl");
+        public static final String XX_LARGE = new String("spacing-b-xxl");
+        public static final String XXX_LARGE = new String("spacing-b-xxxl");
     }
 
     public static class Left {
         public static final String SMALL = new String("spacing-l-s");
         public static final String MEDIUM = new String("spacing-l-m");
         public static final String LARGE = new String("spacing-l-l");
+        public static final String X_LARGE = new String("spacing-l-xl");
+        public static final String XX_LARGE = new String("spacing-l-xxl");
+        public static final String XXX_LARGE = new String("spacing-l-xxxl");
     }
 }
